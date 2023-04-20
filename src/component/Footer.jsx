b1e4633c5bf3755f50889fd6365d32d6982b1b84
Ayushi0516 <ayushigupta0516@gmail.com>
2023-04-20 22:52:48 +0530

    footer added

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,21 +1,25 @@
-import { Box, Flex, Link, SimpleGrid, VStack } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button, Flex, HStack, Link, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import React from "react";
 
 export const Footer = () => {
-    const dummyLink=new Array(12).fill(0).map((_,i)=>`Link ${i+1}`);
+  const dummyLink = new Array(12).fill(0).map((_, i) => `Link ${i + 1}`);
 
   return (
-    
     <VStack py={10} bg="gray.200" mt={4}>
-     <Flex align="center" justify="center">
-        <SimpleGrid column={{base:2,md:3,lg:4}}>
-{dummyLink.map((el)=>(
-    <Link>{el}</Link>
-))}
+      <Flex align="center" justify="center">
+        <SimpleGrid spacing="2rem" column={{ base: 2, md: 3, lg: 4 }}>
+          {dummyLink.map((el) => (
+            <Link>
+            <Button variant={"ghost"}>{el}</Button>
+            </Link>
+          ))}
         </SimpleGrid>
-
-     </Flex>
+      </Flex>
+      <HStack>
+        <Text>&copy:{new Date().getFullYear()} Gotham Coffee Shop</Text>
+        <Text>|</Text>
+        <Text> All right reserved</Text>
+      </HStack>
     </VStack>
-   
-  )
-}
+  );
+};
